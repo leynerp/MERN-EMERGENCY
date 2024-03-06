f3ef4f89f7f58cb0dc30adb1dd778d35d70d0a85
shonder <shonder@gmail.com>
2024-03-06 15:37:37 -0800

    add morgan middleware

--- BACK/app.js
+++ BACK/app.js
@@ -1,7 +1,10 @@
 import express from 'express'
-
+import morgan from 'morgan'
+import bodyParser from 'body-parser'
 export default function initApp () {
-  const app = express()
+  const app = express() 
+  //app.use(bodyParser) 
+  app.use(morgan('dev')) 
   app.listen(3000)
   console.log('listen ')
 }
